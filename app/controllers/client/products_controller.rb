1537872a54d24c339c928cd701c2aba63b362321
“jamalnuman” <jnuman82@gmail.com>
2020-03-07 16:15:36 -0600

    saving prior to github

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -7,7 +7,7 @@ class Client::ProductsController < ApplicationController
   end
 
   def new 
-    render 'new.html.erb'
+    render 'new.html.erb' #the new request is not sending anything to the backend..so the controller action is simple ..the new action is sending a request to the create action 
   end
 
   def create
@@ -18,11 +18,15 @@ class Client::ProductsController < ApplicationController
                     }
 
 
-    repsonse = HTTP.post(
+    response = HTTP.post(
                         "http://localhost:3000/api/products", 
                         form: client_params
                         )
-    render "create.html.erb"
+    #render "create.html.erb"
+    product = response.parse
+    # p product
+    redirect_to "/client/products/#{product["id"]}" #this makes a new webrequest..this will automatically go through the entire cycle one more time and render the show page for the new product by itself 
+
   end
 
   def show
@@ -31,4 +35,33 @@ class Client::ProductsController < ApplicationController
     render 'show.html.erb'
   end
 
+  def edit
+    response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
+    # @recipe_id = params[:id] #the keyword of params only resides in the controller
+    @product = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    client_params = {
+                    name: params[:name],
+                    description: params[:description],
+                    price: params[:price]
+                    }
+
+
+    response = HTTP.patch(
+                        "http://localhost:3000/api/products/#{params[:id]}", #remember to send this id with the update action and to send the correct verb of patch for the update 
+                        form: client_params
+                        )
+    #render "create.html.erb"
+    product = response.parse
+    # p product
+    redirect_to "/client/products/#{product["id"]}" #this makes a new webrequest..this will automatically go through the entire cycle one more time and render the show page for the new product by itself 
+  end
+
+  def destroy
+    response = HTTP.delete("http://localhost:3000/api/products/#{params[:id]}")
+    redirect_to '/client/products' #these are the routes that you are being redirected which will call the controller and then the show/index page
+  end
 end
